3e7d7c17710bd725f1fb1155eee5f3b765622a0d
John L. Villalovos <john@sodarock.com>
2025-05-18 11:58:37 -0700

    chore: enable error reporting for `config.devel.php`
    When doing development it is useful to see all the error messages. The
    `config.dist.php` file still leaves it disabled.

--- config/config.devel.php
+++ config/config.devel.php
@@ -2,8 +2,8 @@
 
 mysqli_report(MYSQLI_REPORT_OFF);
 error_reporting(E_ALL & ~E_NOTICE);
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 /**
  * Application configuration
