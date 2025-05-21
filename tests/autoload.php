246a8c887830d14870adbe56cdfca006d63f8689
John L. Villalovos <john@sodarock.com>
2025-05-20 20:11:37 -0700

    test: default timezone must be 'America/Chicago' for unit tests
    There can be failures in the unit tests at certain times if the
    default timezone in `config.php` isn't "America/Chicago".
    
    Use "America/Chicago" in the CI and fail `phpunit` early if
    `config.php` has a different value.
    
    Closes: #544

--- tests/autoload.php
+++ tests/autoload.php
@@ -10,3 +10,8 @@ require_once(ROOT_DIR . 'tests/TestBase.php');
 
 require_once(ROOT_DIR . 'tests/fakes/namespace.php');
 require_once(ROOT_DIR . 'lib/Common/Helpers/namespace.php');
+
+if (Configuration::Instance()->GetDefaultTimezone() != "America/Chicago") {
+  echo "\nERROR: Default timezone in 'config/config.php' must be 'America/Chicago in order to run unit tests.\n";
+  exit(1);
+}
