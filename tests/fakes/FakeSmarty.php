12b7a0c4a53167fe530211117aad80d1f9db8499
labmecanicatec <58854556+labmecanicatec@users.noreply.github.com>
2025-06-21 19:26:11 -0500

    refactor: Update FakeSmarty to use namespaced Smarty class
    Added 'use Smarty\Smarty;' to import the namespaced Smarty class and updated the getTemplateVars method signature to remove the type hint for $_ptr. This improves compatibility with the current Smarty library.

--- tests/fakes/FakeSmarty.php
+++ tests/fakes/FakeSmarty.php
@@ -4,12 +4,15 @@ if (file_exists(ROOT_DIR . 'vendor/autoload.php')) {
   require_once ROOT_DIR . 'vendor/autoload.php';
 }
 
+use Smarty\Smarty;
+
+
 class FakeSmarty extends Smarty
 {
     public $_LastVar;
     public $_Value;
 
-    public function getTemplateVars($varname = null, Smarty_Internal_Data $_ptr = null, $search_parents = true)
+    public function getTemplateVars($varname = null, $_ptr = null, $search_parents = true)
     {
         $this->_LastVar = $varname;
 
