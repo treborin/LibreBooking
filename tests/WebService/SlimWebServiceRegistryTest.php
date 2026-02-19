84b454a9a18e6479360ff8edf34919c5eb37b58f
John L. Villalovos <john@sodarock.com>
2026-02-19 14:45:03 -0800

    test: enable `strict_types=1` for `tests/WebService/`
    Add `declare(strict_types=1);` to all the tests files in
    `tests/WebService/` and below.

--- tests/WebService/SlimWebServiceRegistryTest.php
+++ tests/WebService/SlimWebServiceRegistryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require_once(ROOT_DIR . 'lib/external/Slim/Slim.php');
 require_once(ROOT_DIR . 'lib/external/Slim/Route.php');
 require_once(ROOT_DIR . 'lib/WebService/Slim/namespace.php');
--- tests/WebService/WebServiceSecurityTest.php
+++ tests/WebService/WebServiceSecurityTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require_once(ROOT_DIR . 'lib/WebService/namespace.php');
 
 class WebServiceSecurityTest extends TestBase
