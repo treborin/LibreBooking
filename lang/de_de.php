1486841378fd1be6879ee3c6ac50cf09e6a9b0d1
AurelioSilver <aureliosilverdawn@gmail.com>
2026-05-31 02:38:50 -0700

    fix: Changes to the date formats for the german translation

--- lang/de_de.php
+++ lang/de_de.php
@@ -5,6 +5,46 @@ require_once('en_gb.php');
 
 class de_de extends en_gb
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return array
+     */
+    protected function _LoadDates()
+    {
+        $dates = parent::_LoadDates();
+
+        // change defaults here
+        $dates['general_date'] = 'd.m.Y';
+        $dates['general_datetime'] = 'd.m.Y H:i:s';
+        $dates['short_datetime'] = 'j.n.y H:i';
+        $dates['schedule_daily'] = 'l, d.m.Y';
+        $dates['reservation_email'] = 'd.m.Y @ H:i (e)';
+        $dates['res_popup'] = 'd.m.Y H:i';
+        $dates['res_popup_time'] = 'H:i';
+        $dates['short_reservation_date'] = 'j.n.y H:i';
+        $dates['dashboard'] = 'l, d.m.Y H:i';
+        $dates['period_time'] = 'H:i';
+        $dates['timepicker'] = 'H:i';
+        $dates['mobile_reservation_date'] = 'd.m. H:i';
+        $dates['general_date_js'] = 'dd.mm.yy';
+        $dates['general_time_js'] = 'H:mm';
+        $dates['timepicker_js'] = 'H:i';
+        $dates['momentjs_datetime'] = 'D.M.YY H:mm';
+        $dates['calendar_time'] = 'H:mm';
+        $dates['calendar_dates'] = 'd M';
+        $dates['embedded_date'] = 'j.n.'; // Numeric day.month without leading zeroes, e.g. 7.3.
+        $dates['embedded_time'] = 'H:i';
+        $dates['embedded_datetime'] = 'j.n. H:i';
+        $dates['report_date'] = '%d.%m.';
+
+        $this->Dates = $dates;
+        return $this->Dates;
+    }
+
     /**
      * @return array
      */
